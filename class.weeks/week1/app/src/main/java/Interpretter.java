80b3526ba4616e11ad294e83f0c32b9dcec84c21
Phil Crow <crow.phil@gmail.com>
2022-08-20 07:22:52 -0500

    simplified adding digits listener

--- class.weeks/week1/app/src/main/java/Interpretter.java
+++ class.weeks/week1/app/src/main/java/Interpretter.java
@@ -2,17 +2,14 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
-import java.util.List;
-
 public class Interpretter extends CalcBaseListener {
     int answer = 0;
 
 	@Override
     public void enterProgram(CalcParser.ProgramContext ctx) {
-        List<TerminalNode> numberNodes = ctx.NUMBER();
-        for (TerminalNode node : numberNodes) {
-            answer += Integer.valueOf(node.getText());
-        }
+        int left = Integer.valueOf(ctx.NUMBER(0).getText());
+        int right = Integer.valueOf(ctx.NUMBER(1).getText());
+        answer = left + right;
     }
 
     public int getAnswer() {
